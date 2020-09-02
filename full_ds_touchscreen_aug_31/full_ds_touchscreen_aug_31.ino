92a85d5afcad0e794f7d5ffcd77fb38cca2dbd9c
Gymnast544 <oberoiarjun2009@gmail.com>
2020-09-01 22:49:15 -0400

    updated stuff

--- full_ds_touchscreen_aug_31/full_ds_touchscreen_aug_31.ino
+++ full_ds_touchscreen_aug_31/full_ds_touchscreen_aug_31.ino
@@ -41,10 +41,24 @@ char pinup[12] = {30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41};//Bytes which 
 
 
 
+volatile short numclocks = 0;
+volatile boolean bitbuffer[20];
+
+
+volatile boolean xbits[16] = {0, 1, 0, 0, 1, 0, 0, 0,    0, 0, 0, 0, 0, 0, 0, 0};
+volatile boolean ybits[16] = {0, 0, 1, 1, 1, 0, 0, 0,    0, 0, 0, 0, 0, 0, 0, 0};
+volatile boolean bbits[16] = {0, 0, 0, 1, 0, 1, 1, 1,    1, 0, 0, 0, 0, 0, 0, 0};
+
+volatile short xpos = 128; 
+volatile short ypos = 128;
+
+
+
+
 void resetbuttons() {
   //Setting all button modes to input
   for(byte i=0;i<12;i++){
-    pinMode(pins[i], INPUT);
+    pinMode(pins[i], INPUT_PULLUP);
   }
 
   //Resets the status of each pin
@@ -57,20 +71,23 @@ void resetbuttons() {
 void setup() {
   // put your setup code here, to run once:
   resetbuttons();
+  setupTouchscreenPins();
+  attachTouchscreenInterrupts();
   Serial.begin(115200);
 
 }
 
-void loop() {
+FASTRUN void loop() {
   // put your main code here, to run repeatedly:
-  serialInterface();
-  pinMode(DPAD_RIGHT_PIN, OUTPUT);
-  digitalWrite(DPAD_RIGHT_PIN, LOW);
-  delay(500);
-  digitalWrite(DPAD_RIGHT_PIN, HIGH);
-  delay(500);
+  //serialInterface();
   //Serial.println(digitalRead(DPAD_RIGHT_PIN));
   //delay(500);
+
+  digitalWrite(PEN, HIGH);
+  delay(500);
+  digitalWrite(PEN, LOW);
+  delay(500);
+  Serial.println(digitalReadFast(CS_PIN));
 }
 
 
@@ -122,5 +139,111 @@ void checkMode(int incomingbyte) {
     //Performs the "handshake test"
     //If it receives byte 100, it will send byte 101 - this is the verification that the desktop program uses to verify that it's an Input Interface connected
     Serial.write(101);
+  }
+}
+
+
+void setupTouchscreenPins() {
+  // put your setup code here, to run once:
+
+
+  pinMode(MOSI_PIN, INPUT);
+  pinMode(MISO_PIN, INPUT);
+  pinMode(PEN, OUTPUT);
+  digitalWrite(PEN, HIGH);
+  pinMode(CS_PIN, INPUT);
+  pinMode(SYNC, INPUT);
+  Serial.begin(115200);
+  Serial.println("Starting...");
+  while(digitalReadFast(SYNC)==1){
+    //wait until the next frame
+  }
+}
+
+
+void attachTouchscreenInterrupts(){
+  //attachInterrupt(SYNC, syncinterrupt, FALLING);
+  attachInterrupt(CS_PIN, CSfall, FALLING);
+}
+
+
+
+FASTRUN void CSfall() {
+  attachInterrupt(CS_PIN, CSrise, RISING);
+  attachInterrupt(SCK_PIN, clockchanging, RISING);
+  pinMode(MISO_PIN, OUTPUT);
+  digitalWriteFast(MISO_PIN, LOW);
+  //Serial.println("F");
+}
+
+
+FASTRUN void CSrise() {
+  attachInterrupt(CS_PIN, CSfall, FALLING);
+  detachInterrupt(SCK_PIN);
+  pinMode(MISO_PIN, INPUT);
+  //Serial.println("R");
+  /*
+  for(int i=0;i<8;i++){
+    Serial.print(bitbuffer[i]);
+  }*/
+  numclocks = 0;
+}
+
+
+FASTRUN void clockchanging() {
+  bitbuffer[numclocks] = digitalReadFast(MOSI_PIN);
+  //Serial.print();
+  //digitalWriteFast(MISO_PIN, bitbuffer[numclocks]);
+  numclocks++;
+  if(numclocks>7){
+    numclocks = 0;
+    if(bitbuffer[3]){
+      //it's an x or y pos thingy
+      if(bitbuffer[1]){
+        //x pos
+        digitalWriteFast(MISO_PIN, xbits[0]);
+        attachInterrupt(SCK_PIN, clockchangex, FALLING);
+      }else{
+        //y pos
+        digitalWriteFast(MISO_PIN, ybits[0]);
+        attachInterrupt(SCK_PIN, clockchangey, FALLING);
+      }
+    }else if(bitbuffer[5]){
+      //it's a beginning or end CS cycle
+      digitalWriteFast(MISO_PIN, bbits[0]);
+      attachInterrupt(SCK_PIN, clockchangeb, FALLING);
+    }else{
+      //don't do anything
+      detachInterrupt(SCK_PIN);
+    }
+  }
+}
+
+
+
+//the clockchange x, y, and b functions are pretty much the same, with the exception that they pull data from a different list, for efficiency
+FASTRUN void clockchangex(){
+  digitalWriteFast(MISO_PIN, xbits[numclocks]);
+  numclocks++;
+  if(numclocks>15){
+    numclocks = 0;
+  }
+}
+
+
+FASTRUN void clockchangey(){
+  digitalWriteFast(MISO_PIN, ybits[numclocks]);
+  numclocks++;
+  if(numclocks>15){
+    numclocks = 0;
+  }
+}
+
+
+FASTRUN void clockchangeb(){
+  digitalWriteFast(MISO_PIN, bbits[numclocks]);
+  numclocks++;
+  if(numclocks>15){
+    numclocks = 0;
   }
 }
