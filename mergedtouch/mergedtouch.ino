0b03025e013b2887d7000782076e3904bc25170e
Gymnast544 <oberoiarjun2009@gmail.com>
2020-11-07 20:27:43 -0500

    new serial protocol working tm

--- mergedtouch/mergedtouch.ino
+++ mergedtouch/mergedtouch.ino
@@ -59,6 +59,8 @@ struct frameData{
 
 int pins[12] = {A_PIN, B_PIN, X_PIN, Y_PIN, DPAD_LEFT_PIN, DPAD_RIGHT_PIN, DPAD_UP_PIN, DPAD_DOWN_PIN, LEFT_SHOULDER_PIN, RIGHT_SHOULDER_PIN, START_PIN, SELECT_PIN};
 
+#define firstthreebitmask 7
+
 
 volatile short numclocks = 0;
 volatile byte numpolls = 0;
@@ -69,7 +71,105 @@ volatile boolean xbits[16] = {0, 0, 0, 0, 0, 0, 0, 0,    0, 0, 0, 0, 0, 0, 0, 0}
 volatile boolean ybits[16] = {0, 1, 1, 1, 1, 1, 1, 1,    0, 0, 0, 0, 0, 0, 0, 0};
 volatile boolean bbits[16] = {0, 0, 0, 1, 0, 1, 1, 1,    1, 0, 0, 0, 0, 0, 0, 0};
 
+void FASTRUN executeFrameOld(frameData Frame){
+  memcpy(xbits, Frame.framexbits, 16);//Tested and works - reassigns the array correctly 
+  memcpy(ybits, Frame.frameybits, 16);
+}
 void FASTRUN executeFrame(frameData Frame){
+  digitalWriteFast(PEN, Frame.pen);
+  //Can't just digitalWrite to the button pins, each line is required (can't use a loop because I'm using digitalWriteFast)
+  if(Frame.a){
+    //button isn't pressed
+    pinMode(A_PIN, INPUT);
+  }else{
+    pinMode(A_PIN, OUTPUT);
+    digitalWriteFast(A_PIN, LOW);
+  }
+  if(Frame.b){
+    //button isn't pressed
+    pinMode(B_PIN, INPUT);
+  }else{
+    pinMode(B_PIN, OUTPUT);
+    digitalWriteFast(B_PIN, LOW);
+  }
+  if(Frame.x){
+    //button isn't pressed
+    pinMode(X_PIN, INPUT);
+  }else{
+    pinMode(X_PIN, OUTPUT);
+    digitalWriteFast(X_PIN, LOW);
+  }
+  if(Frame.y){
+    //button isn't pressed
+    pinMode(Y_PIN, INPUT);
+  }else{
+    pinMode(Y_PIN, OUTPUT);
+    digitalWriteFast(Y_PIN, LOW);
+  }
+  if(Frame.startbutton){
+    //button isn't pressed
+    pinMode(START_PIN, INPUT);
+  }else{
+    pinMode(START_PIN, OUTPUT);
+    digitalWriteFast(START_PIN, LOW);
+  }
+  if(Frame.selectbutton){
+    //button isn't pressed
+    pinMode(SELECT_PIN, INPUT);
+  }else{
+    pinMode(SELECT_PIN, OUTPUT);
+    digitalWriteFast(SELECT_PIN, LOW);
+  }
+  if(Frame.l){
+    //button isn't pressed
+    pinMode(LEFT_SHOULDER_PIN, INPUT);
+  }else{
+    pinMode(LEFT_SHOULDER_PIN, OUTPUT);
+    digitalWriteFast(LEFT_SHOULDER_PIN, LOW);
+  }
+  if(Frame.r){
+    //button isn't pressed
+    pinMode(RIGHT_SHOULDER_PIN, INPUT);
+  }else{
+    pinMode(RIGHT_SHOULDER_PIN, OUTPUT);
+    digitalWriteFast(RIGHT_SHOULDER_PIN, LOW);
+  }
+  if(Frame.du){
+    //button isn't pressed
+    pinMode(DPAD_UP_PIN, INPUT);
+  }else{
+    pinMode(DPAD_UP_PIN, OUTPUT);
+    digitalWriteFast(DPAD_UP_PIN, LOW);
+  }
+  if(Frame.dd){
+    //button isn't pressed
+    pinMode(DPAD_DOWN_PIN, INPUT);
+  }else{
+    pinMode(DPAD_DOWN_PIN, OUTPUT);
+    digitalWriteFast(DPAD_DOWN_PIN, LOW);
+  }
+  if(Frame.dl){
+    //button isn't pressed
+    pinMode(DPAD_LEFT_PIN, INPUT);
+  }else{
+    pinMode(DPAD_LEFT_PIN, OUTPUT);
+    digitalWriteFast(DPAD_LEFT_PIN, LOW);
+  }
+  if(Frame.dr){
+    //button isn't pressed
+    pinMode(DPAD_RIGHT_PIN, INPUT);
+  }else{
+    pinMode(DPAD_RIGHT_PIN, OUTPUT);
+    digitalWriteFast(DPAD_RIGHT_PIN, LOW);
+  }
+  if(Frame.lid){
+    //button isn't pressed
+    pinMode(LID, INPUT);
+  }else{
+    pinMode(LID, OUTPUT);
+    digitalWriteFast(A_PIN, LOW);
+  }
+  //TODO: IMPLEMENT POWER
   memcpy(xbits, Frame.framexbits, 16);//Tested and works - reassigns the array correctly 
   memcpy(ybits, Frame.frameybits, 16);
 }
@@ -199,7 +299,7 @@ FASTRUN void clockchangeb(){
   }
 }
 
-
+/*
 FASTRUN void loop() {
   if(Serial.available()>0){
     byte inbyte = Serial.read();
@@ -258,4 +358,117 @@ FASTRUN void loop() {
     }
   }
   
+}*/
+FASTRUN void loop() {
+  if(Serial.available()>0){
+    byte inByte = Serial.read();
+    Serial.write("X");
+    Serial.println(int(inByte));
+    if (inByte==245){
+      Serial.write("Y");
+      frameData inputFrame;
+      //starting with a data transfer, starting with buttons
+      boolean transferring = true;
+      while(transferring==true){
+        if(Serial.available()>0){
+          inByte = Serial.read();
+          byte identifier = inByte & firstthreebitmask;
+          if(identifier==7){
+            //move onto the next section
+            Serial.write("d");
+            transferring=false;
+          }else if(identifier == 0){
+            //first data byte
+            //ranges from 3 to 8
+            inputFrame.a = bitRead(inByte, 3);
+            inputFrame.b = bitRead(inByte, 4);
+            inputFrame.x = bitRead(inByte, 5);
+            inputFrame.y = bitRead(inByte, 6);
+            inputFrame.dl = bitRead(inByte, 7);
+            Serial.write("a");
+          }else if(identifier == 4){
+            //second data byte
+            inputFrame.dr = bitRead(inByte, 3);
+            inputFrame.du = bitRead(inByte, 4);
+            inputFrame.dd = bitRead(inByte, 5);
+            inputFrame.l = bitRead(inByte, 6);
+            inputFrame.r = bitRead(inByte, 7);
+            Serial.write("b");
+          }else if(identifier == 2){
+            //third data byte
+            inputFrame.startbutton = bitRead(inByte, 3);
+            inputFrame.selectbutton = bitRead(inByte, 4);
+            inputFrame.lid = bitRead(inByte, 5);
+            inputFrame.pen = bitRead(inByte, 6);
+            inputFrame.pwr = bitRead(inByte, 7);
+            Serial.write("c");
+          }
+        }
+      }
+      Serial.write("y");
+      //moving onto touchscreen
+      transferring = true;
+      while(transferring==true){
+        if(Serial.available()>0){
+          inByte = Serial.read();
+          byte identifier = inByte & firstthreebitmask;
+          if(inByte==255){
+            //finishing the transfer
+            Serial.write("k");
+            transferring=false;
+          }else if(identifier == 0){
+            //first data byte
+            inputFrame.framexbits[0] = bitRead(inByte, 3);
+            inputFrame.framexbits[1] = bitRead(inByte, 4);
+            inputFrame.framexbits[2] = bitRead(inByte, 5);
+            inputFrame.framexbits[3] = bitRead(inByte, 6);
+            inputFrame.framexbits[4] = bitRead(inByte, 7);
+            Serial.write("e");
+          }else if(identifier == 4){
+            //second data byte
+            inputFrame.framexbits[5] = bitRead(inByte, 3);
+            inputFrame.framexbits[6] = bitRead(inByte, 4);
+            inputFrame.framexbits[7] = bitRead(inByte, 5);
+            inputFrame.framexbits[8] = bitRead(inByte, 6);
+            inputFrame.framexbits[9] = bitRead(inByte, 7);
+            Serial.write("f");
+          }else if(identifier == 2){
+            //third data byte
+            inputFrame.framexbits[10] = bitRead(inByte, 3);
+            inputFrame.framexbits[11] = bitRead(inByte, 4);
+            inputFrame.framexbits[12] = bitRead(inByte, 5);
+            inputFrame.framexbits[13] = bitRead(inByte, 6);
+            inputFrame.framexbits[14] = bitRead(inByte, 7);
+            Serial.write("g");
+          }else if(identifier == 6){
+            //fourth data byte
+            inputFrame.frameybits[0] = bitRead(inByte, 3);
+            inputFrame.frameybits[1] = bitRead(inByte, 4);
+            inputFrame.frameybits[2] = bitRead(inByte, 5);
+            inputFrame.frameybits[3] = bitRead(inByte, 6);
+            inputFrame.frameybits[4] = bitRead(inByte, 7);
+            Serial.write("h");
+          }else if(identifier == 1){
+            //fifth data byte
+            inputFrame.frameybits[5] = bitRead(inByte, 3);
+            inputFrame.frameybits[6] = bitRead(inByte, 4);
+            inputFrame.frameybits[7] = bitRead(inByte, 5);
+            inputFrame.frameybits[8] = bitRead(inByte, 6);
+            inputFrame.frameybits[9] = bitRead(inByte, 7);
+            Serial.write("i");
+          }else if(identifier == 5){
+            //sixth data byte
+            inputFrame.frameybits[10] = bitRead(inByte, 3);
+            inputFrame.frameybits[11] = bitRead(inByte, 4);
+            inputFrame.frameybits[12] = bitRead(inByte, 5);
+            inputFrame.frameybits[13] = bitRead(inByte, 6);
+            inputFrame.frameybits[14] = bitRead(inByte, 7);
+            Serial.write("j");
+          }
+        }
+      }
+      Serial.write("x");
+      executeFrame(inputFrame);
+    }
+  }
 }
